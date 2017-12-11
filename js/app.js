09602523d7cfbe9fa119d40035f44055da8b9271
Thomas Vallez <thomvallez@users.noreply.github.com>
2017-12-11 22:48:10 +0100

    CSS & SASS

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 import config from './config.js'
+import css from './app.scss'
+
+console.log(css);
 
 document.getElementById('button').addEventListener('click', function() {
   // jQuery here
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,23 @@ const path = require('path');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const dev = process.env.NODE_ENV === "dev";
 
+let cssLoaders = [
+  'style-loader',
+  { loader: 'css-loader',
+    options: { importLoaders: 1 }
+  }
+]
+
+if (!dev) {
+  cssLoaders.push({ loader: 'postcss-loader',
+    options: { plugins: (loader) => [
+      require('autoprefixer')({
+        browsers: ['last 2 versions', 'ie > 8']
+      }),
+    ] }
+  })
+}
+
 let config = {
   entry: './js/app.js',
   watch: dev,
@@ -17,6 +34,17 @@ let config = {
         test: /\.js*$/,
         exclude: /(node_modules|bower_components)/,
         use: ['babel-loader']
+      },
+      {
+        test: /\.css*$/,
+        use: cssLoaders
+      },
+      {
+        test: /\.scss*$/,
+        use: [
+          ...cssLoaders,
+          'sass-loader'
+        ]
       }
     ]
   },
