630e3051887c50ea89716b47f84b4c0603c9114c
Thomas Vallez <thomvallez@users.noreply.github.com>
2017-12-11 17:57:06 +0100

    Minification and Lazy Loading

--- js/app.js
+++ js/app.js
@@ -1,11 +1,8 @@
 import config from './config.js'
-import $ from 'jquery'
-import log from './log.js'
 
-let a = "Salut les gens"
-let [b,,c] = [1,2,4,5]
-log(b)
-
-console.log(config);
-
-$('body').css('backgroundColor', '#FFF');
+document.getElementById('button').addEventListener('click', function() {
+  // jQuery here
+  import('jquery').then(($) => {
+    $('body').css('backgroundColor', '#DDD');
+  })
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
 const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const dev = process.env.NODE_ENV === "dev";
 
-module.exports = {
+let config = {
   entry: './js/app.js',
-  watch: true,
+  watch: dev,
   output: {
     path: path.resolve('./dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: "./dist/"
   },
+  devtool: dev ? "cheap-module-eval-source-map" : "source-map",
   module: {
     rules: [
       {
@@ -15,5 +19,15 @@ module.exports = {
         use: ['babel-loader']
       }
     ]
-  }
+  },
+  plugins: [
+  ]
 }
+
+if (!dev) {
+  config.plugins.push(new UglifyJSPlugin({
+    sourceMap: true
+  }))
+}
+
+module.exports = config
